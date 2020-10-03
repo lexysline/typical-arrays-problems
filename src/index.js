ec2180c76b2d8da1403c86a527ab821bebbe18d6
SashaJET <sashajet.mail@gmail.com>
2020-10-03 21:29:58 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    const result = Math.min(...array);
+    return result;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    const result = Math.max(...array);
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || array.length == 0) {
+        return 0;
+    }
+    const tolal = array.reduce((sum, current) => sum + current, 0);
+    const result = tolal / array.length;
+    return result;
+};
